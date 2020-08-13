042de18cc28d2437fb0858a04a23b2f806fc0c18
Anish Ojha <anishojha82@gmail.com>
2020-08-14 02:08:35 +0530

    sample test for newsapi through voice commands

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const alanKey = '4d6fc89287be4e05f7bc3914a62b57b02e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
 
+    const [news, setNews] = useState([]);
+
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command }) => {
-                if (command === 'testCommand') {
-                    alert('Executed');
+            onCommand: ({ command, savedArticles }) => {
+                if (command === 'headlines') {
+                    setNews(savedArticles);
                 }
             }
-        })
-    }, [])
+        });
+    }, []);
 
     return (
         <div>

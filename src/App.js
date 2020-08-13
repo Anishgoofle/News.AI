5d3c69b5f081ccab24ccd8a489467301a22a0da3
Anish Ojha <anishojha82@gmail.com>
2020-08-14 01:47:06 +0530

    test phase for ALAN-AI app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
+const alanKey = '4d6fc89287be4e05f7bc3914a62b57b02e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if (command === 'testCommand') {
+                    alert('Executed');
+                }
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1>News AI</h1>

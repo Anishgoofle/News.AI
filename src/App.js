cdd59722ae16394f60cf6588d559c800ed85cd36
Anish Ojha <anishojha82@gmail.com>
2020-08-14 18:13:29 +0530

    added css + other ai term for search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
-
+import useStyles from './styles';
 const alanKey = '4d6fc89287be4e05f7bc3914a62b57b02e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    const classes = useStyles();
 
     const [news, setNews] = useState([]);
 
@@ -22,7 +24,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>News AI</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+            </div>
             <NewsCards articles={news} />
         </div>
     )
